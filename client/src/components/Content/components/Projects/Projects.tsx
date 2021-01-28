d8097f9f990dbd4d453aa86462aa350db36b4217
emirodriguezalc <rodriguez.gimenez.emilia@gmail.com>
2021-01-28 20:33:32 +0100

    full functional flip component

--- client/src/components/Content/components/Projects/Projects.tsx
+++ client/src/components/Content/components/Projects/Projects.tsx
@@ -7,12 +7,31 @@ interface Props {
 }
 
 export const Projects: React.FC<Props> = () => {
+  const fakeData = [
+    {
+      mediaSrc: '/assets/projects/hamster-project-1.jpg',
+      sourceCodeLink: 'https://github.com/emirodriguezalc/todo-list',
+      gifs: 'this is supposed to be a video',
+    },
+    {
+      mediaSrc: '/assets/projects/hamster-project-2.jpg',
+      sourceCodeLink: 'https://github.com/emirodriguezalc/dream-car',
+      gifs: 'this is supposed to be a video',
+    },
+    {
+      mediaSrc: '/assets/projects/hamster-project-3.jpg',
+      sourceCodeLink: 'https://github.com/emirodriguezalc/pokedex',
+      gifs: 'this is supposed to be a video',
+    },
+  ];
   return (
     <section className="Projects">
       <div id="projects" className="content-placeholder" />
       <h1>Projects</h1>
       <section>
-        <ProjectBox />
+        {fakeData.map((e) => (
+          <ProjectBox mediaSrc={e.mediaSrc} sourceCodeLink={e.sourceCodeLink} gifs={e.gifs} />
+        ))}
       </section>
     </section>
   );
--- client/src/components/Content/components/Projects/components/projectBox/ProjectBox.tsx
+++ client/src/components/Content/components/Projects/components/projectBox/ProjectBox.tsx
@@ -1,10 +1,15 @@
+/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 import './ProjectBox.scss';
 import ReactCardFlip from 'react-card-flip';
 
-interface Props {}
+interface Props {
+  mediaSrc:string,
+  sourceCodeLink:string,
+  gifs:string
+}
 
-export const ProjectBox: React.FC<Props> = () => {
+export const ProjectBox: React.FC<Props> = ({ mediaSrc, sourceCodeLink, gifs }) => {
   const [isFlipped, setIsFlipped] = useState(false);
 
   const handleClick = (e: any) => {
@@ -13,18 +18,36 @@ export const ProjectBox: React.FC<Props> = () => {
   };
 
   return (
-    <ReactCardFlip isFlipped={isFlipped}>
-      <article role="presentation" onClick={handleClick}>
-        <img src="/assets/projects/hamster-project-2.jpg" alt="" />
-      </article>
-      <article role="presentation" onClick={handleClick}>
-        <div className="">
-          <img src="/assets/projects/hamster-project-2.jpg" alt="" />
-          <button type="button">SHOW PREVIEW</button>
-          <button type="button">LINK TO SOURCE CODE</button>
-        </div>
-      </article>
-    </ReactCardFlip>
+    <section className="project-container">
+      <ReactCardFlip isFlipped={isFlipped}>
+        <article role="presentation" onClick={handleClick} className="project-front">
+          <img
+            // src="/assets/projects/hamster-project-2.jpg"
+            src={mediaSrc}
+            className="project-container-media"
+            alt=""
+          />
+        </article>
+        <article role="presentation" onClick={handleClick} className="project-back">
+          <img
+            src={mediaSrc}
+            className="project-container-media back"
+            alt=""
+          />
+          <button type="button" className={`project-button ${gifs}`}>
+            Take a look
+          </button>
+          <button type="button" onClick={() => window.open(sourceCodeLink)} className="project-button github">
+            Go to source code
+            <img
+              src="/assets/projects/github.png"
+              alt="github"
+              className="project-button-icon"
+            />
+          </button>
+        </article>
+      </ReactCardFlip>
+    </section>
   );
 };
 
