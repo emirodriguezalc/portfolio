551bd4f16e8c9bc214aecfee1f80e74dbc714629
emirodriguezalc <rodriguez.gimenez.emilia@gmail.com>
2021-01-29 17:04:04 +0100

    chnge path.join and routes

--- server/server.js
+++ server/server.js
@@ -22,11 +22,9 @@ app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(`${__dirname}/client/build`));
-  app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, '..', 'client', 'build', 'index.html'),
-    );
+  app.use('/', express.static(path.join(__dirname, '../client/build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
 
