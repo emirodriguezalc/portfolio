c1958a5366e73ea3be88e96e5fd28a440887867a
emirodriguezalc <rodriguez.gimenez.emilia@gmail.com>
2021-01-29 15:34:12 +0100

    added again the get * with a fowardslash in front of it!

--- server/server.js
+++ server/server.js
@@ -21,10 +21,9 @@ app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/build'));
-/*   app.get('*', (req, res) => {
-    console.log(path.resolve(__dirname, '../client/build', 'index.html'));
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-  }); */
+  });
 }
 
 const transporter = nodemailer.createTransport({
