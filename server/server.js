25be9f3389ecd6f5c42e36ff9254a444a2137ffc
emirodriguezalc <rodriguez.gimenez.emilia@gmail.com>
2021-01-29 16:27:29 +0100

    added .. to path.resolve

--- server/server.js
+++ server/server.js
@@ -24,7 +24,9 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(`${__dirname}/client/build`));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(
+      path.resolve(__dirname, '..', 'client', 'build', 'index.html'),
+    );
   });
 }
 
