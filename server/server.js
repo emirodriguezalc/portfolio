6a3f3e7a74e623e272262c20b6ca08bb0a3aadc4
emirodriguezalc <rodriguez.gimenez.emilia@gmail.com>
2021-01-29 16:20:03 +0100

    added homepage and dirname in server static

--- server/server.js
+++ server/server.js
@@ -17,11 +17,12 @@ const PORT = process.env.PORT || 8080;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
+
 app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static(`${__dirname}/client/build`));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
