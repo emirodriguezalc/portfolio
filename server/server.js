9cae7f37d5c4afd384a8e687f375c1bbe602ae2e
emirodriguezalc <rodriguez.gimenez.emilia@gmail.com>
2021-01-29 16:17:35 +0100

    added root and modified server.js

--- server/server.js
+++ server/server.js
@@ -21,11 +21,9 @@ app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, '..', 'client', 'build', 'index.html'),
-    );
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
