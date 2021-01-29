5c8099e8cf89ed32d85824a5955eeaabe0a3e359
emirodriguezalc <rodriguez.gimenez.emilia@gmail.com>
2021-01-29 15:05:39 +0100

    babel is bad

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ import nodemailer from 'nodemailer';
 import multiparty from 'multiparty';
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
